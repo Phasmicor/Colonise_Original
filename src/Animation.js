3ea1a0a00d2783be7b3def78ea36b07d984dae12
Phasmicor <phasmicor@gmail.com>
2015-02-14 17:04:58 +0000

    Level loading (clunky version)
    Can’t figure out a cleaner way of doing it

--- src/Animation.js
+++ src/Animation.js
@@ -7,10 +7,13 @@ var AnimationLayer = cc.Layer.extend({
 	init:function(){
 		this._super();
 		
-		
+		//Load level data (I can't figure out how to iterate through the different res objects - like res.L1, res.L2 
 
-		cc.loader.loadJson(res.L1, function(error, data){
-			cc.log(JSON.stringify(data)); //data is the json object
+		var level = 1 //have to do it this way instead...
+		
+		cc.loader.loadJson("res/Levels/level" + level + ".json", function(error, data){ //load json file for level - level[0].Y = 361
+			var info = JSON.stringify(data); // [{"Y": "361", "X": "61", "type": "def", "number": "0", "team": "0"},
+			cc.log(info);
 		});		
 
 	},
